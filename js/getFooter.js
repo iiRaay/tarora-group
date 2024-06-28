372e5f3095b28a8db2a28e9d673ace9ba62cea22
Raymond Duong <iiraay@hotmail.com>
2024-06-27 22:11:21 -0600

    format all files with prettier

--- js/getFooter.js
+++ js/getFooter.js
@@ -2,12 +2,12 @@
 
 // Function to load and insert the element content
 function getFooter() {
-    fetch('../footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-element').innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading element:', error);
-        });
+  fetch("../footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-element").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error loading element:", error);
+    });
 }
