05a5f8f4e463c20a11aa2cf25b9bf7f7db3db6a7
lambrendan <lam_brendan@hotmail.com>
2024-11-19 23:20:45 -0700

    Added Black Friday Deal

--- js/Modal.js
+++ js/Modal.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded',function(){
 
     var anniversaryModal = new bootstrap.Modal(document.getElementById('anniversaryModal'));
-    var stillModal = new bootstrap.Modal(document.getElementById('still-images-popup'));
-    var drawModal = new bootstrap.Modal(document.getElementById('drawModal'));
-    const drawImage = document.getElementById('draw-still') 
+    //var stillModal = new bootstrap.Modal(document.getElementById('still-images-popup'));
+    //var drawModal = new bootstrap.Modal(document.getElementById('drawModal'));
+    //const drawImage = document.getElementById('draw-still') 
     anniversaryModal.show(); 
-
+/*
     setTimeout(function(){
       anniversaryModal.hide();
       stillModal.show()
@@ -14,4 +14,5 @@ document.addEventListener('DOMContentLoaded',function(){
         drawModal.show();
         stillModal.hide();
     });
+*/
 });
